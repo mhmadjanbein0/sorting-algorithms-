d9f8011e9fd273241a1e0ad2011e55a78068ce7d
mhmad janbein <101193564+mhmadjanbein0@users.noreply.github.com>
2026-01-23 16:19:32 +0000

    Add tree.c source file

---
+++ tree.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+typedef struct nd 
+{
+    int val;
+    struct nd *left,*right;
+}node;
+typedef struct list 
+{
+    int val;
+    struct list *next;
+}list;
+int power(int base, int exp)
+{
+    if(exp==0)
+        return base;
+    return base*power(base,exp-1);
+}
+void BUILD_TREE(node *r,list *ls,int height)
+{
+    if(highL<0)return;
+    node *r=(node*)malloc(sizeof(node));
+    if(height==0&&ls!=NULL)
+    {
+        (*r)->val=ls->val;
+         ls=ls->next;
+    }
+    else
+          (*r)->val=-1;
+        
+    BUILD_TREE(&((*R)->Left), L, height - 1);
+    BUILD_TREE(&((*R)->Right), L, height - 1);
+}
+int main()
+{
+    printf("hello word");
+}
